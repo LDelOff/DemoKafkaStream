f7bde10866bbaa0f541f7d0902ca0b258470480b
L_DelOff <l_deloff@ldeloff.ru>
2024-06-01 11:35:30 +0300

    First commit

---
+++ src/test/java/ru/ldeloff/demokafkastream/DemoKafkaStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ldeloff.demokafkastream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoKafkaStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
